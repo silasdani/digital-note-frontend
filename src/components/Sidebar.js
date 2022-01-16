83c7d175be3891b776a4e94a3f02091ba801c1d3
silasdani <danielsilas23@yahoo.com>
2022-01-16 17:34:12 +0200

    added some animation & sidebar options

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,24 +1,27 @@
 import React from 'react'
-import { AiOutlineVideoCameraAdd, AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
-import { IoIosNotificationsOutline } from "react-icons/io";
-import { ImPencil2 } from "react-icons/im";
+import { AiOutlineVideoCameraAdd, AiOutlineMenu, AiOutlineMessage } from "react-icons/ai";
+import { IoMdSettings } from "react-icons/io";
+import { ImPencil2, ImBook, ImCommand } from "react-icons/im";
 
 const Sidebar = ({ onHamburger }) => {
     return (
-        <div className="absolute bg-black bg-opacity-50 z-40 w-screen h-screen">
-            <div className="sidebar">
+        <div className="flex flex-row">
+            <div className="sidebar transition transform hover:-translate-x-10 motion-reduce:transition-none motion-reduce:transform-none" >
                 <div className="h-14 text-white flex justify-start items-center space-x-2">
                     <button className="mr-6"><AiOutlineMenu color="white" className="w-6 h-6" onClick={onHamburger} /></button>
                     <div className="py-2 bg-red-700 rounded-xl w-10 flex items-center justify-center"><ImPencil2 color="white" /></div>
                     <span className="text-xl text-white">Testud.init</span>
                 </div>
                 <div className="text-white space-y-2">
-                    <div className="h-10 bg-opacity-60 flex justify-center items-center">2</div>
-                    <div className="h-10 bg-opacity-60 flex justify-center items-center">3</div>
+                    <div className="h-10 bg-opacity-60 flex justify-start items-center"><ImBook className="mx-6" /> MY COURSES</div>
+                    <div className="h-10 bg-opacity-60 flex justify-start items-center"><AiOutlineMessage className="mx-6" />MESSAGES</div>
+                    <div className="h-10 bg-opacity-60 flex justify-start items-center"><ImCommand className="mx-6" />ASSIGNMENTS</div>
+                    <div className="h-10 bg-opacity-60 flex justify-start items-center"><IoMdSettings className="mx-6" />SETTINGS</div>
+                    <div className="h-10 bg-opacity-60 flex justify-start items-center"><AiOutlineVideoCameraAdd className="mx-6" />MEET</div>
                 </div>
             </div>
-        </div>
-    )
+            <button onClick={onHamburger} className=" bg-black bg-opacity-50 w-4/5" />
+        </div>)
 }
 
 export default Sidebar;
