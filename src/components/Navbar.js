68b65ebf1870f389c8bea60a894fa10b754d4c71
silasdani <danielsilas23@yahoo.com>
2022-01-23 23:54:31 +0200

    added video link on nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 import { AiOutlineVideoCameraAdd, AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
 import { IoIosNotificationsOutline } from "react-icons/io";
 import { ImPencil2 } from "react-icons/im";
@@ -6,21 +7,22 @@ import gravatar from 'gravatar'
 import { connect } from 'react-redux';
 
 const Navbar = ({ onHamburger, user: { email } }) => {
+    const [visible, setVisibility] = useState(false);
     return (
         <div className="navbar">
             <div className="h-14 text-white flex justify-center items-center space-x-2">
                 <button className="mr-6"><AiOutlineMenu color="white" className="w-6 h-6" onClick={onHamburger} /></button>
                 <div className="py-2 bg-red-700 rounded-xl w-10 flex items-center justify-center"><ImPencil2 color="white" /></div>
-                <span className="text-xl text-white">Testud.init</span>
+                <span className="text-xl text-white">Digital.init</span>
             </div>
             <div className="h-14 w-2/5 flex justify-center items-center">
                 <input className="input bg-opacity-40 px-4 " placeholder='Search' />
                 <button className="bg-gray-400 w-20 h-10 flex items-center justify-center"><AiOutlineSearch color="white" className="w-6 h-6" /></button>
             </div>
             <div className="h-14 text-white flex items-center space-x-4">
-                <button className="h-10 px-2 hover:bg-gradient-to-t hover:bg-gray-900 rounded-full"><AiOutlineVideoCameraAdd className="w-8 h-8" /></button>
+                <Link to="/room" className="h-10 px-2 hover:bg-gradient-to-t hover:bg-gray-900 rounded-full"><AiOutlineVideoCameraAdd className="w-8 h-8" /></Link>
                 <button className="h-10 px-2 hover:bg-gradient-to-t hover:bg-gray-900 rounded-full"><IoIosNotificationsOutline className="w-8 h-8" /></button>
-                <button className="w-14 hover:opacity-40"><img className="w-10 h-10 rounded-full" src={gravatar.url(email)} /></button>
+                <button className="w-14 hover:opacity-40" onClick={() => setVisibility(!visible)}><img className="w-10 h-10 rounded-full" src={gravatar.url(email)} /></button>
             </div>
         </div>
     )
