eb1af44d7ba7bb06d9ddf9cf5f81b61f606fdc46
silasdani <danielsilas23@yahoo.com>
2022-01-25 12:01:01 +0200

    all

--- src/api/ApiService.js
+++ src/api/ApiService.js
@@ -3,39 +3,31 @@ import axios from "axios";
 // import store from "../redux/store";
 
 class ApiService {
-  get(url, mapFunc = () => { }) {
+  async get(url, mapFunc = () => { }) {
     let headers = config();
     // store.dispatch(showSpinner());
 
-    return axios.get(url, headers).then((response) => {
-      // store.dispatch(hideSpinner());
-      return mapFunc(response);
-    });
+    const response = await axios.get(url, headers);
+    return mapFunc(response);
   }
 
-  post(url, body, mapFunc, params = {}) {
+  async post(url, body, mapFunc, params = {}) {
     // store.dispatch(showSpinner());
 
-    return axios
-      .post(url, body, params)
-      .then((response) => {
-        // store.dispatch(hideSpinner());
-        return mapFunc(response);
-      });
+    const response = await axios
+      .post(url, body, params);
+    return mapFunc(response);
   }
 
-  patch(url, body, mapFunc, params = {}) {
+  async patch(url, body, mapFunc, params = {}) {
     // store.dispatch(showSpinner());
 
-    return axios
-      .patch(url, body, params)
-      .then((response) => {
-        // store.dispatch(hideSpinner());
-        return mapFunc(response);
-      });
+    const response = await axios
+      .patch(url, body, params);
+    return mapFunc(response);
   }
 
-  delete(url, params = {}) {
+  async delete(url, params = {}) {
     // store.dispatch(showSpinner());
 
     let headers = { ...params, ...config() };
@@ -45,10 +37,7 @@ class ApiService {
         ...params.headers,
       };
     }
-    return axios.delete(url, headers)
-      .then(() => {
-        // store.dispatch(hideSpinner());
-      });
+    await axios.delete(url, headers);
   }
 }
 
