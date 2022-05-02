c71020c05241ad9ee44fa2acab00da4b11f6cc8f
silasdani <danielsilas23@yahoo.com>
2022-05-02 17:10:02 +0300

    test

--- src/pages/ExamListPage.js
+++ src/pages/ExamListPage.js
@@ -18,7 +18,7 @@ const ExamListPage = () => {
                     <thead className="bg-white">
                       <tr>
                         <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
-                          # Exam Name
+                          # Exam Name (112)
                         </th>
                         <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
                           Exam key
